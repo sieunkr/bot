ef222615f2377a084652c5ef59baf12c232d3cc4
sieunkim <sieunkr@gmail.com>
2019-03-07 23:00:34 +0900

    미세먼지 메시지 전달 로직 구현

--- telegram-spring/src/main/java/eddy/bot/telegram/TelegramApplication.java
+++ telegram-spring/src/main/java/eddy/bot/telegram/TelegramApplication.java
@@ -1,9 +1,15 @@
 package eddy.bot.telegram;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 import org.telegram.telegrambots.*;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
@@ -11,8 +17,19 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.List;
+
 @SpringBootApplication
 public class TelegramApplication implements CommandLineRunner {
+
+    @Value("${broker.airs.url}")
+    private String brokerAirsUrl;
+
+    private final RestTemplate restTemplate;
+
+    public TelegramApplication(RestTemplateBuilder builder) {
+        this.restTemplate = builder.build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TelegramApplication.class, args);
@@ -29,15 +46,81 @@ public class TelegramApplication implements CommandLineRunner {
             @Override
             public void onUpdateReceived(Update update) {
                 if (update.hasMessage() && update.getMessage().hasText()) {
-                    SendMessage message = new SendMessage()
-                            .setChatId(update.getMessage().getChatId())
-                            .setText(update.getMessage().getText());
 
-                    try {
-                        execute(message); // Call method to send the message
-                    } catch (TelegramApiException e) {
-                        e.printStackTrace();
+
+                    //TODO:컴포넌트 및 메서드 분리
+                    String stringMessage = update.getMessage().getText();
+                    SendMessage message = new SendMessage();
+
+                    //TODO:
+                    if(stringMessage.equals("미세먼지")){
+
+                        try {
+
+                            ResponseEntity<List<AirDto>> rateResponse =
+                                    restTemplate.exchange(brokerAirsUrl,
+                                            HttpMethod.GET, null, new ParameterizedTypeReference<List<AirDto>>() {
+                                            });
+                            List<AirDto> airs = rateResponse.getBody();
+
+                            StringBuilder stringBuilder = new StringBuilder();
+                            for (AirDto air : airs) {
+                                stringBuilder.append(air.getStationName())
+                                                .append(" ")
+                                                .append("미세먼지:")
+                                                .append(air.getPm10Grade())
+                                                .append("\r\n");
+                            }
+
+                            message.setChatId(update.getMessage().getChatId())
+                                    .setText(stringBuilder.toString());
+                            execute(message);
+
+                        } catch (TelegramApiException e) {
+                            e.printStackTrace();
+                        }
+
+                        /*
+                        //TODO:Reactive Programming
+                        WebClient webClient = WebClient.create();
+                        webClient.get().uri(brokerAirsUrl)
+                                .retrieve()
+                                .bodyToFlux(AirDto.class)
+                                .collectList()
+                                .subscribe(
+                                    //consumer
+                                    air -> {
+                                        message.setChatId(update.getMessage().getChatId())
+                                                .setText("미세먼지 정보를 전달합니다.");
+                                        try {
+                                            execute(message); // Call method to send the message
+                                        } catch (TelegramApiException e) {
+                                            e.printStackTrace();
+                                        }
+                                    },
+                                    //error
+                                    null,
+                                    //complete
+                                    () -> {
+
+                                    });
+                        */
+
+
                     }
+                    else{
+                        message.setChatId(update.getMessage().getChatId())
+                                .setText("네?");
+
+                        try {
+                            execute(message); // Call method to send the message
+                        } catch (TelegramApiException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+
+
                 }
             }
 
@@ -48,7 +131,7 @@ public class TelegramApplication implements CommandLineRunner {
 
             @Override
             public String getBotToken() {
-                return "발급받은토큰";
+                return "비밀";
             }
 
         });
